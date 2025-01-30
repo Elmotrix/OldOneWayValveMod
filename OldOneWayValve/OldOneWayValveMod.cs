066f19835992711ed5718c307648057c0409245e
Elmotrix <kim.hoftvedt@gmail.com>
2025-01-30 20:05:01 +0100

    Add project files.

---
+++ OldOneWayValve/OldOneWayValveMod.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Scripts.Objects;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldOneWayValve
+{
+    [HarmonyPatch(typeof(Prefab), "LoadCorePrefabs")]
+    public class AddStructureIntoKit
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            MultiConstructor pipeKit = Prefab.Find<MultiConstructor>("ItemPipeValve");
+            Structure oldValve = Prefab.Find<Structure>("StructurePipeOneWayValve");
+            if (pipeKit != null && oldValve != null)
+            {
+                pipeKit.Constructables.Add(oldValve);
+                oldValve.BuildStates[0].Tool.ToolEntry = pipeKit;
+            }
+            MultiConstructor liquidipeKit = Prefab.Find<MultiConstructor>("ItemLiquidPipeValve");
+            Structure oldLiquidValve = Prefab.Find<Structure>("StructureLiquidPipeOneWayValve");
+            if (liquidipeKit != null && oldLiquidValve != null)
+            {
+                liquidipeKit.Constructables.Add(oldLiquidValve);
+                oldLiquidValve.BuildStates[0].Tool.ToolEntry = liquidipeKit;
+            }
+        }
+    }
+}
